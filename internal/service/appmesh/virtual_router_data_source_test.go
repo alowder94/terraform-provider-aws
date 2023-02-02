40336fdbe2aff833a7ebdb94f4cb193a687d768b
alowder94 <alowder94@gmail.com>
2023-02-02 17:23:20 -0600

    Continuing test - Still WIP

--- internal/service/appmesh/virtual_router_data_source_test.go
+++ internal/service/appmesh/virtual_router_data_source_test.go
@@ -19,6 +19,25 @@ func TestAccAppMeshVirtualRouterDataSource_basic(t *testing.T) {
 		PreCheck:                 func() { acctest.PreCheck(t); acctest.PreCheckPartitionHasService(appmesh.EndpointsID, t) },
 		ErrorCheck:               acctest.ErrorCheck(t, appmesh.EndpointsID),
 		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
-		CheckDestroy: 			  testAccCheckVirtualRouterDestroy(ctx),
+		CheckDestroy:             testAccCheckVirtualRouterDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVirtualRouterConfig(virtualRouterName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrPair(resourceName, "arn", dataSourceName, "arn"),
+					resource.TestCheckResourceAttrPair(resourceName, "created_date", dataSourceName, "created_name"),
+					resource.TestCheckResourceAttrPair(resourceName, "last_updated_date", dataSourceName, "last_updated_date"),
+					resource.TestCheckResourceAttrPair(resourceName, "mesh_name", dataSourceName, "mesh_name"),
+					resource.TestCheckResourceAttrPair(resourceName, "mesh_owner", dataSourceName, "mesh_owner"),
+					resource.TestCheckResourceAttrPair(resourceName, "name", dataSourceName, "name"),
+					resource.TestCheckResourceAttrPair(resourceName, "resource_owner", dataSourceName, "resource_owner"),
+					resource.TestCheckResourceAttrPair(resourceName, "spec.0.listener.0.port", dataSourceName, "spec.0.listeer.0.port"),
+					resource.TestCheckResourceAttrPair(resourceName, "spec.0.listener.0.protocol", dataSourceName, "spec.0.listener.0.protocol"),
+					resource.TestCheckResourceAttrPair(resourceName, "tags", dataSourceName, "tags"),
+				),
+			},
+		},
 	})
 }
+
+//Define testAccVirtualRouterConfig Here
